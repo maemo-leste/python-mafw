413a7f983f0648445af398ffb299aa6c96c67bce
lizardo <lizardo@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2009-07-28 16:14:43 +0000

    python-mafw: merge MafwRegistry fixes from andrea's branch
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@1265 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- tests/test_mafw_registry.py
+++ tests/test_mafw_registry.py
@@ -1,11 +1,33 @@
 import unittest
-
+import gobject
 import mafw
 
+class MySourcePlugin(mafw.Source):
+    __gtype_name__ = 'MySourcePlugin'
+    
+class MyRendererPlugin(mafw.Renderer):
+    __gtype_name__ = 'MyRendererPlugin'
+  
 class TestMafwRegistry(unittest.TestCase):
     def test_get_instance(self):
         reg = mafw.Registry.get_instance()
         self.assertTrue(isinstance(reg, mafw.Registry))
+    
+    def test_get_sources(self):
+        reg = mafw.Registry.get_instance()
+        x1 = gobject.new(MySourcePlugin, uuid = 'MySourcePlugin1')
+        x2 = gobject.new(MySourcePlugin, uuid = 'MySourcePlugin2')
+        reg.add_extension(x1)
+        reg.add_extension(x2)
+        self.assertEquals(set(reg.get_sources()), set([x1, x2]))
+    
+    def test_get_renderers(self):
+        reg = mafw.Registry.get_instance()
+        x1 = gobject.new(MyRendererPlugin, uuid = 'MyRendererPlugin1')
+        x2 = gobject.new(MyRendererPlugin, uuid = 'MyRendererPlugin2')
+        reg.add_extension(x1)
+        reg.add_extension(x2)
+        self.assertEquals(set(reg.get_renderers()), set([x1, x2]))
 
 if __name__ == "__main__":
     unittest.main()
