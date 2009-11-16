2a148cc099c01df085cc2f21a11a50c19d098e97
lmoura <lmoura@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2009-11-16 20:12:59 +0000

    Improving mafw registry tests
    Using proper uuid
    
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@1450 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- tests/test_mafw_registry.py
+++ tests/test_mafw_registry.py
@@ -1,6 +1,7 @@
 import unittest
 import gobject
 import mafw
+from uuid import uuid4
 
 class MySourcePlugin(mafw.Source):
     __gtype_name__ = 'MySourcePlugin'
@@ -9,32 +10,39 @@ class MyRendererPlugin(mafw.Renderer):
     __gtype_name__ = 'MyRendererPlugin'
 
 class TestMafwRegistry(unittest.TestCase):
+
+
+    def tearDown(self):
+        reg = mafw.Registry.get_instance()
+
+        for extension in reg.get_sources() + reg.get_renderers():
+            reg.remove_extension(extension)
+
     def test_get_instance(self):
         reg = mafw.Registry.get_instance()
         self.assertTrue(isinstance(reg, mafw.Registry))
 
     def test_get_sources(self):
         reg = mafw.Registry.get_instance()
-        x1 = gobject.new(MySourcePlugin, uuid = 'MySourcePlugin1')
-        x2 = gobject.new(MySourcePlugin, uuid = 'MySourcePlugin2')
+        x1 = gobject.new(MySourcePlugin, name='source1', uuid = uuid4())
+        x2 = gobject.new(MySourcePlugin, uuid = uuid4(), name='source2')
         reg.add_extension(x1)
         reg.add_extension(x2)
         self.assertEquals(set(reg.get_sources()), set([x1, x2]))
 
     def test_get_renderers(self):
         reg = mafw.Registry.get_instance()
-        x1 = gobject.new(MyRendererPlugin, uuid = 'MyRendererPlugin1')
-        x2 = gobject.new(MyRendererPlugin, uuid = 'MyRendererPlugin2')
+        x1 = gobject.new(MyRendererPlugin, uuid = uuid4(), name='renderer1')
+        x2 = gobject.new(MyRendererPlugin, uuid = uuid4(), name='renderer2')
         reg.add_extension(x1)
         reg.add_extension(x2)
-        self.assertEquals(set(reg.get_renderers()), set([x1, x2]))
+        x = reg.get_renderers()
+        self.assertEquals(set(x), set([x1, x2]))
 
     def test_list_plugins(self):
-        # FIXME: test currently fails because list_plugins() is always
-        # returning a empty list (even on C).
         reg = mafw.Registry.get_instance()
-        x1 = gobject.new(MyRendererPlugin, uuid = 'MyPlugin1')
-        x2 = gobject.new(MySourcePlugin, uuid = 'MyPlugin2')
+        x1 = gobject.new(MyRendererPlugin, uuid = uuid4(), name='list1')
+        x2 = gobject.new(MySourcePlugin, uuid =uuid4(), name='list2')
         reg.add_extension(x1)
         reg.add_extension(x2)
         self.assertEquals(set(reg.list_plugins()), set([x1, x2]))
